2239405ca3e8c18e3a456f8d23ee4848bf49c417
Chandra Shettigar <chandrakanth.shettigar@gmail.com>
2020-12-24 02:05:48 -0600

    add port-forward option for 'cloudcli ssh' command

--- dist/cloudcli.sh
+++ dist/cloudcli.sh
@@ -10,16 +10,35 @@ DOT_SSH="${DOT_SSH:-$PWD/.cloudcli/ssh}"
 DOT_DOCTL="${DOT_DOCTL:-$PWD/.cloudcli/doctl}"
 AWS_REGION="${AWS_REGION:-us-west-2}"
 HOSTNAME_ALIAS="${HOSTNAME_ALIAS:-devteds-cloudcli}"
+PORT_FORWARDS=""
+
 
 show_help() {
   echo "
 Usage: 
-  cloudcli exec|ssh|version [COMMAND]
+  cloudcli ssh|version|exec [OPTIONS] [COMMAND]
 
 Examples:
+  cloudcli ssh -h
+  cloudcli ssh -p 8085:8000 -p 8086:8001
   cloudcli exec aws s3 ls
-  cloudcli exec kubectl apply -f MANIFEST.YAML"
+  cloudcli exec kubectl get po
+  cloudcli version"
   exit 1
+}
+
+show_ssh_usage() {
+  echo "
+Usage: 
+  cloudcli ssh [OPTIONS]
+
+Options:
+  -p <host-port:port>     Port forward from host-to-container
+  -h                      Help
+
+Examples:
+  cloudcli ssh
+  cloudcli ssh -p 8085:8000 -p 8086:8001"  
 }
 
 [ ! -n "$1" ] && show_help
@@ -36,12 +55,28 @@ function docker_run {
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e HOSTNAME_ALIAS=${HOSTNAME_ALIAS} \
     -e AWS_REGION=${AWS_REGION} \
+    $PORT_FORWARDS \
     $CLOUDCLI_DOCKER_IMAGE \
     $CMD
 }
 
 function cli_exec {
   docker_run "$CLOUDCLI_CMD"
+}
+
+function parse_ssh_opts {
+  while [ "$1" != "" ]; do
+      case $1 in
+          -p | --port-forward )
+              shift
+              PORT_FORWARDS="$PORT_FORWARDS -p $1 "
+          ;;
+        -h | --help )    show_ssh_usage
+            exit
+        ;;          
+      esac
+      shift
+  done
 }
 
 CLOUDCLI_CMD_TYPE=$1
@@ -55,6 +90,7 @@ if [ "$CLOUDCLI_CMD_TYPE" = "exec" ]; then
   fi
   docker_run "$CLOUDCLI_CMD"
 elif [ "$CLOUDCLI_CMD_TYPE" = "ssh" ]; then
+  parse_ssh_opts $CLOUDCLI_CMD
   docker_run
 elif [ "$CLOUDCLI_CMD_TYPE" = "version" ]; then
   docker_run "/cloudcli-home/bin/version.sh"
